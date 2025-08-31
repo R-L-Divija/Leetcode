bde7fcbd03c2c44772af05654c9f20da47fcc747
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2025-08-31 15:31:47 +0530

    Time: 1 ms (71.93%), Space: 40.3 MB (93.81%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.java
+++ 2083-three-divisors/2083-three-divisors.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isThree(int n) {
         int c=0;
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<n;i++){
             if(n%i==0)
             c++;
         }
-        if(c==3)return true;
+        if(c==1)return true;
         return false;
     }
 }
