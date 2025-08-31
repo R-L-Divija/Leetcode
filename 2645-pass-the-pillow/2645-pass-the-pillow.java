e3a34d08f37666c28a1c7656a4ba838d81cb5373
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2025-08-31 11:39:51 +0530

    Time: 0 ms (100%), Space: 40.7 MB (31.49%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -1,10 +1,14 @@
 class Solution {
     public int passThePillow(int n, int t) {
-        t %= (n - 1) * 2;
-        if (t < n) {
+  
+        
+         t %= (n - 1) * 2;//cycle pass
+         if (t < n) {
             return 1 + t;
-        } else {
-            return n - (t - (n - 1));
-        }
+        } 
+        return n-(t-(n-1));
+
+        
+          
     }
 }
