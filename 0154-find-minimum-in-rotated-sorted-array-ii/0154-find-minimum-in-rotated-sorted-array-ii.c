9bb9c5ca99d9b9779f6ffc649effdd87d2cbd123
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2025-09-02 04:28:24 +0530

    Time: 71 ms (2.54%), Space: 8.5 MB (45.18%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.c
@@ -0,0 +1,12 @@
+int findMin(int* a, int n) {
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                int t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+    return a[0];
+}
