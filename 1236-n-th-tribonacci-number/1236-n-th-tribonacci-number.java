3ed29322bfc9e0d2978a9ba13517e0e6e6296256
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2025-09-01 07:48:24 +0530

    Time: 0 ms (100%), Space: 40.6 MB (27.44%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int tribonacci(int n) {
+     if(n==0)return 0;
+      if(n==1||n==2)return 1;
+    
+     /*return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);*/
+     int t1=0;
+     int t2=1;
+     int t3=1;
+     for(int i=3;i<=n;i++){
+        int temp=t1+t2+t3;
+        t1=t2;
+        t2=t3;
+        t3=temp;
+     }
+     return t3;
+    }
+}
