b4db60506dd440814d6c7666eff796497a299bfa
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2025-09-01 08:54:34 +0530

    Time: 0 ms (100%), Space: 40.4 MB (66.05%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+    if(n==1)return 1;
+    else if(n==2)return 2;
+    else{
+        int n1=1,n2=2,n3=0;
+        for(int i=3;i<=n;i++){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+        }
+        return n3;
+    }
+
+    }
+}
