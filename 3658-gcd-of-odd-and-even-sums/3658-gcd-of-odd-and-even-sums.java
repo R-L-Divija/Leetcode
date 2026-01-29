3ba80fd9f3c9466480f73cafd6bf00df07834aec
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-01-29 05:33:38 +0530

    Time: 3 ms (24.5%), Space: 42.6 MB (50.69%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int gcd(int a,int b){
+        if(b==0)return a;
+        else return gcd(b,a%b);
+    }
+    public int gcdOfOddEvenSums(int n) {
+        int sO=0;
+        int sE=0;
+       for(int i=1;i<=2*n;i++){
+if(i%2!=0)sO+=i;
+else sE+=i;
+       }
+       
+       return gcd(sO,sE);
+    }
+}
