5ae8cb60254cf95a8539f6eab4da2ab6ce46d2bf
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-09 11:58:46 +0530

    Time: 106 ms (1.43%), Space: 44.3 MB (35.56%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,22 @@
 class Solution {
-    public void gss(int[]a,int index,ArrayList<Integer>ans,List<List<Integer>>res){
-        if(a.length==index){
-            res.add(new ArrayList<>(ans));
-            return;
-        }
-        ans.add(a[index]);
-        gss(a,index+1,ans,res);
-        ans.remove(ans.size()-1);
-        gss(a,index+1,ans,res);
+    List<List<Integer>>ans;
+    List<Integer>temp;
+    public void func(int index,int[] arr){
+if(index==arr.length){
+    ans.add(new ArrayList<>(temp));
+    return;
+}
+   //take
+   temp.add(arr[index]);
+   func(index+1,arr);
+   temp.remove(temp.size()-1);
+   //not take
+   func(index+1,arr);
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>>res=new ArrayList<>();
-        gss(nums,0,new ArrayList<>(),res);
-        return res;
-    
+        ans=new ArrayList<>();
+        temp=new ArrayList<>();
+        func(0,nums);
+        return ans;
     }
 }
