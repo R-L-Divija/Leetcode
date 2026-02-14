ba2a0269d7ddaf2fd313fb9aaca5a2522c696add
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-14 15:00:04 +0530

    Time: 1 ms (100%), Space: 46.3 MB (84.85%) - LeetHub

---
+++ 3833-count-dominant-indices/3833-count-dominant-indices.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int dominantIndices(int[] nums) {
+        int c=0;
+        int sum=0;
+
+        for(int x:nums){
+            sum+=x;
+        }
+        for(int i=0;i<nums.length;i++){
+         sum-=nums[i];
+         int n=nums.length-i-1;
+        if(n>0){
+         if((long)nums[i]*n>sum)c++;
+        }
+        }
+        return c;
+    }
+}
