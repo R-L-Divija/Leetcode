db7f26cb744bccb151fb485847bb6be5415d18d5
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-21 04:56:45 +0530

    Time: 10 ms (85.86%), Space: 50.3 MB (56.95%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,35 +1,32 @@
 class Solution {
-
-    public boolean ship(int arr[],int d,int cap){
+    public boolean func(int[] a,int d,int m){
         int l=0;
-        int days=1;
-        for(int x:arr){
-            if(l+x >cap){
-                days++;
-                l=x;}
-                else{
-                    l+=x;
-                }
+        int day=1;
+        for(int x:a){
+            if(l+x<=m){
+                l+=x;
+            }else{
+                day++;
+                l=x;
+            }
         }
-        return days<=d;
+        return day<=d;
     }
-    public int shipWithinDays(int[] arr, int d) {
-      int l=0;
-      int h=0;
-      int ans=-1;
-      for(int x:arr){
-        l=Math.max(l,x);
-        h+=x;
-      }  
-      while(l<=h){
-        int m=(l+h)/2;
-        if(ship(arr,d,m)){
-            ans=m;
-            h=m-1;
-        }else{
-            l=m+1;
+    public int shipWithinDays(int[] weights, int days) {
+        int l=0;
+        int h=0;
+        for(int x:weights){
+            l=Math.max(l,x);
+            h+=x;
         }
-      }
-      return ans;
+        while(l<h){
+            int m=l+(h-l)/2;
+            if(func(weights,days,m)){
+                h=m;
+            }else{
+                l=m+1;
+            }
+        }
+return l;
     }
 }
