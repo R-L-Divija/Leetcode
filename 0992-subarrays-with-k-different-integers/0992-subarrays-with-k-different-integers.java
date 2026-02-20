988d1aba3badc9ae36e5c0e8bc0b3c32ebb49d09
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-20 13:23:26 +0530

    Time: 51 ms (31.58%), Space: 50.4 MB (38.42%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,20 +1,20 @@
 class Solution {
-       public int count(int[] a, int k) {
-        int frq[]=new int[a.length+1];
-        int c=0;
-        int ans=0;
+    public int count(int[] nums,int k){
         int l=0;
-        for(int r=0;r<a.length;r++){
-            frq[a[r]]++;
-            if(frq[a[r]]==1)c++;
-            while(c>k){
-                frq[a[l]]--;
-                if(frq[a[l]]==0)c--;
+        int r=0;
+        int c=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        while(r<nums.length){
+            map.put(nums[r],map.getOrDefault(nums[r],0)+1);
+            while(map.size()>k){
+                map.put(nums[l],map.get(nums[l])-1);
+                if(map.get(nums[l])==0)map.remove(nums[l]);
                 l++;
             }
-            ans+=r-l+1;
+c+=r-l+1;
+r++;
         }
-        return ans;
+        return c;
     }
     public int subarraysWithKDistinct(int[] nums, int k) {
         return count(nums,k)-count(nums,k-1);
