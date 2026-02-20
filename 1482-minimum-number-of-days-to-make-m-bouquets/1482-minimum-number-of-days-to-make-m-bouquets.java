1853e2b54b97aa2cb19baf9944c65f0bd3311492
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-20 21:00:24 +0530

    Time: 16 ms (91.74%), Space: 82.3 MB (95.08%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean func(int[] d,int m,int k,int c){
+        int b=0;
+        int cou=0;
+        for(int x:d){
+            if(x<=c){
+                cou++;
+                if(cou==k){
+                    b++;
+                 cou=0;
+                }
+            }else{
+                cou=0;
+            }
+        }
+        return b>=m;
+    }
+    public int minDays(int[] d, int m, int k) {
+        if((long)m*k>d.length)return -1;
+        int l=1;
+        int h=0;
+        for(int x:d){
+            
+            h=Math.max(h,x);
+        }
+        while(l<h){
+            int mid=l+(h-l)/2;
+            if(func(d,m,k,mid)){
+                h=mid;
+            }else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+}
