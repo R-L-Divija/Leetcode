1ea24ce9510888815607be520f981cd08411cfc7
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-02-28 12:01:41 +0530

    Time: 10 ms (85.48%), Space: 48.3 MB (97.41%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int networkDelayTime(int[][] edges, int V, int src) {
+        ArrayList<ArrayList<int[]>>adj=new ArrayList<>();
+        for(int i=0;i<=V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            int x=edges[i][0];
+            int y=edges[i][1];
+            int wt=edges[i][2];
+            adj.get(x).add(new int[]{y,wt});
+        }
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        int dis[]=new int[V+1];
+        Arrays.fill(dis,Integer.MAX_VALUE);
+        dis[src]=0;
+        pq.offer(new int[]{0,src});
+        while(!pq.isEmpty()){
+            int []t=pq.poll();
+            int dis1=t[0];
+            int node=t[1];
+            if(dis1>dis[node])continue;
+            for(int []x:adj.get(node)){
+                int val=x[0];
+                int wt=x[1];
+                if(wt+dis1<dis[val]){
+                    dis[val]=wt+dis1;
+                    pq.offer(new int[]{dis[val],val});
+                }
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=1;i<=V;i++){
+            max=Math.max(max,dis[i]);
+        }
+        if(max==Integer.MAX_VALUE)return -1;
+        return max;
+    }
+}
