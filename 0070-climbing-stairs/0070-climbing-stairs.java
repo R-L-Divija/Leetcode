55e6d0a8adcd4425877a84c175be6096cd7a4bb7
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-04 19:36:23 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,19 @@
 class Solution {
-    int cs(int index,int n,int dp[]){
-        if(index==n)return 1;
-        if(index>n)return 0;
-        if(dp[index]!=0)return dp[index];
-        return dp[index]=cs(index+1,n,dp)+cs(index+2,n,dp);
-    }
+    // int cs(int index,int n,int dp[]){
+    //     if(index==n)return 1;//base case
+    //     if(index>n)return 0;//pruning case
+    //     if(dp[index]!=0)return dp[index];//cache check
+    //     return dp[index]=cs(index+1,n,dp)+cs(index+2,n,dp);//transitions store and return 
+    // }
     public int climbStairs(int n) {
+    // int dp[]=new int[n+1]; //store number of ways to reach n from i
+    // return cs(0,n,dp);
     int dp[]=new int[n+1];
-    return cs(0,n,dp);
+    dp[0]=1;
+    dp[1]=2;
+    for(int i=2;i<n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n-1];
     }
 }
