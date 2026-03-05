5ec6bb29fa9cd914c7b5c175df76de32e8f224fd
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-05 19:18:02 +0530

    Time: 9 ms (43.53%), Space: 42.3 MB (30.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,7 @@
 class Solution {
     public int fib(int n) {
-        int dp[]=new int[n+1];
         if(n<=1)return n;
-        if(dp[n]!=0)return dp[n];
-        return dp[n]=fib(n-1)+fib(n-2);
+        //logic
+        return fib(n-1)+fib(n-2);
     }
 }
