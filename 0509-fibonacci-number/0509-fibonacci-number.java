6e395411dbe6d112ff3fbee6d56f4564a7ee7c91
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-05 19:42:50 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.8%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,28 @@
 class Solution {
 
-    int func(int n){
+ //1)Memorization
+ 
+//  =>base case
+//  =>Pruning case
+//  =>Cache check
+//  =>transitions states
+//  n->(n-1) &(n-2)
+//  =>store ans return 
+
+
+    int func(int n,int dp[]){
         //base
         if(n<=1)return n;
+        //pruning case --
+
+        //cache check
+       if(dp[n]!=-1)return dp[n];
         //logic
-        return func(n-1)+func(n-2);
+        return dp[n]=func(n-1,dp)+func(n-2,dp);
     }
     public int fib(int n) {
-     return func(n);
+     int dp[]=new int[n+1];
+     Arrays.fill(dp,-1);
+     return func(n,dp);
     }
 }
