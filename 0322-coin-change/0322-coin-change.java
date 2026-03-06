0e50a6f1fc36dc32e07b07e4cb772694ed199cdc
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-06 14:58:29 +0530

    Time: 23 ms (33.68%), Space: 46.9 MB (38.39%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int coinChange(int[] a, int t) {
+      int n=a.length;
+      if(t==0)return 0;
+      int dp[][]=new int[a.length][t+1];
+      for(int i=0;i<a.length;i++){
+       Arrays.fill(dp[i],Integer.MAX_VALUE);
+      }
+      for(int r=0;r<n;r++){
+        dp[r][0]=0;
+      }
+    for(int j=0;j<=t;j++){
+        if(j % a[n-1] == 0){
+                dp[n-1][j] = j / a[n-1];
+            }
+        }
+      for(int i=n-2;i>=0;i--){
+        for(int j=0;j<=t;j++){
+            //not take
+            int nt=dp[i+1][j];
+            //take;
+            int ta=Integer.MAX_VALUE;
+            if(j-a[i]>=0 &&dp[i][j-a[i]]!=Integer.MAX_VALUE){
+            ta=1+dp[i][j-a[i]];
+            }
+            dp[i][j]=Math.min(ta,nt);
+        }
+      }
+      if(dp[0][t]>=Integer.MAX_VALUE)return -1;
+      
+      return dp[0][t];
+    }
+}
