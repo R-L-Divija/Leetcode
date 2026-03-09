09e5db6bf729c9fe227333b962d36187228022fd
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-09 19:33:45 +0530

    Time: 4 ms (18.89%), Space: 49.9 MB (6.62%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,38 +1,40 @@
 class Solution {
     public int findCircleNum(int[][] a) {
+        int n=a.length;
+        int m=a[0].length;
         ArrayList<ArrayList<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<a.length;i++){
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<a.length;i++){
-            for(int j=i+1;j<a.length;j++){
-           if(a[i][j]==1){
-            adj.get(i).add(j);
-           adj.get(j).add(i);}
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
+                if(a[i][j]==1){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
         }
-        }
-        int c=0;
-        int v[]=new int[a.length];
+        int v[]=new int[n];
         Arrays.fill(v,0);
         Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<a.length;i++){
-            if(v[i]==0){
-                c++;
-            q.offer(i);
-            v[i]=1;
+    ;
+       int c=0;
+       for(int i=0;i<n;i++){
+        if(v[i]==0){
+        q.offer(i);
+           v[i]=1;
+            c++;
             while(!q.isEmpty()){
-                int val=q.poll();
-                for(int x:adj.get(val)){
+                int t=q.poll();
+                for(int x:adj.get(t)){
                     if(v[x]==0){
                         q.offer(x);
                         v[x]=1;
                     }
                 }
             }
-            
-            }
-
         }
-        return c;
+       }
+       return c;
     }
 }
