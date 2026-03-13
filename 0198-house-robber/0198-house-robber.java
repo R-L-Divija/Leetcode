d1c9b64cad166bc82fe8ef8b7f48eaecc0595091
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-03-14 02:44:50 +0530

    Time: 0 ms (100%), Space: 43 MB (32.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,30 @@
 class Solution {
-    int func(int index,int n,int arr[],int dp[]){
+    // int func(int index,int n,int arr[],int dp[]){
         
-        if(index>=n) return 0;
+    //     if(index>=n) return 0;
 
-        if(dp[index]!=-1) return dp[index];
+    //     if(dp[index]!=-1) return dp[index];
 
-        int take = arr[index] + func(index+2,n,arr,dp);
+    //     int take = arr[index] + func(index+2,n,arr,dp);
 
-        int nottake = func(index+1,n,arr,dp);
+    //     int nottake = func(index+1,n,arr,dp);
 
-        return dp[index] = Math.max(take,nottake);
-    }
+    //     return dp[index] = Math.max(take,nottake);
+    // }
 
     public int rob(int[] nums) {
         int dp[] = new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return func(0,nums.length,nums,dp);
+        // Arrays.fill(dp,-1);
+        // return func(0,nums.length,nums,dp);
+        if(nums.length==1)return nums[0];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[1],nums[0]);
+
+        for(int i=2;i<nums.length;i++){
+            int m1=nums[i]+dp[i-2];
+            int m2=dp[i-1];
+            dp[i]=Math.max(m1,m2);
+        }
+        return dp[nums.length-1];
     }
 }
