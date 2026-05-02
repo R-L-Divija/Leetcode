5599237deb7195ed13da27ef50705612d683c693
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-05-02 19:26:52 +0530

    Time: 0 ms (100%), Space: 42.2 MB (35.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,12 +8,36 @@ class Solution {
     public int climbStairs(int n) {
     // int dp[]=new int[n+1]; //store number of ways to reach n from i
     // return cs(0,n,dp);
-    int dp[]=new int[n+1];
-    dp[0]=1;
-    dp[1]=2;
-    for(int i=2;i<n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-    return dp[n-1];
+    // int dp[]=new int[n+1];
+    // dp[0]=1;
+    // dp[1]=2;
+    // for(int i=2;i<n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
+    // }
+    // return dp[n-1];
+
+
+
+
+
+
+
+int dp[]=new int[n+1];
+dp[0]=1;
+dp[1]=2;
+for(int i=2;i<n;i++){
+    dp[i]=dp[i-1]+dp[i-2];
+}
+return dp[n-1];
+
+
+
+
+
+
+
+
+
+
     }
 }
