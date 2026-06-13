56e14d51fad535994b964a0eada2a4e02f717e60
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-13 10:39:10 +0530

    Time: 2 ms (96.85%), Space: 46.4 MB (81.55%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -1,21 +1,18 @@
 class Solution {
-    public int calc(String temp,int[]weights){
-        int sum=0;
-        for(char x:temp.toCharArray()){
-            sum+=weights[x-'a'];
-        }
-        return sum%26;
+   public int func(String s,int weights[]){
+    int sum=0;
+    for(char x:s.toCharArray()){
+        sum+=weights[x-'a'];
     }
+    return (int)(sum%26);
+   }
     public String mapWordWeights(String[] words, int[] weights) {
-        String arr[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-        
-        int l=0;
-        StringBuilder temp=new StringBuilder();
-        for(int i=0;i<words.length;i++){
-            int t=calc(words[i],weights);
-         
-            temp.append(arr[25-t]);
-        }
-        return temp.toString();
+       String arr[]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+       StringBuilder temp=new StringBuilder();
+       for(int i=0;i<words.length;i++){
+       int t=func(words[i],weights);
+       temp.append(arr[25-t]);
+       }
+       return temp.toString();
     }
 }
