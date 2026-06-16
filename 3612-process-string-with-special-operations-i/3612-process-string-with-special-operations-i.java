91d17976f1ec2b6f280c28438b611fad44139d92
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-16 08:54:01 +0530

    Time: 4 ms (76.47%), Space: 54.9 MB (80.88%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String processStr(String s) {
+     StringBuilder s1=new StringBuilder();
+     for(char x:s.toCharArray()){
+        if(x=='*'){
+            if(s1.length()>0){
+                s1.deleteCharAt(s1.length()-1);
+            }
+        }
+        else if(x=='#'){
+             if(s1.length()>0){
+                //char l=s1.charAt(s1.length()-1);
+                s1.append(s1);
+            }
+        }
+        else if(x=='%'){
+            if(s1.length()>0){
+                s1.reverse();
+            }
+            }
+            else{
+                s1.append(x);
+            }
+     }
+     return s1.toString();   
+    }
+}
