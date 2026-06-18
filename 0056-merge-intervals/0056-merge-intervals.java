9116020236cc1addebbcd8eb7e7d534228a0b904
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-18 15:46:17 +0530

    Time: 8 ms (90.99%), Space: 49.3 MB (25.7%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,16 +1,21 @@
 class Solution {
     public int[][] merge(int[][] arr) {
-        Arrays.sort(arr,(a,b)->a[0]-b[0]);
-        ArrayList<int[]>res=new ArrayList<>();
-        res.add(new int[]{arr[0][0],arr[0][1]});
-        for(int i=0;i<arr.length;i++){
-            int last[]=res.get(res.size()-1);
-            if(last[1]>=arr[i][0]){
-                last[1]=Math.max(last[1],arr[i][1]);
-            }else{
-                 res.add(new int[]{arr[i][0],arr[i][1]});
-            }
+       Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
+       ArrayList<int[]>p=new ArrayList<>();
+       int ps=arr[0][0];
+       int pe=arr[0][1];
+
+       for(int i=1;i<arr.length;i++){
+        if(arr[i][0]<=pe){
+         pe=Math.max(pe,arr[i][1]);
+        }else{
+            p.add(new int[]{ps,pe});
+             ps=arr[i][0];
+             pe=arr[i][1];
         }
-return res.toArray(new int[res.size()][]);
+       }
+        p.add(new int[]{ps,pe});
+
+return p.toArray(new int[p.size()][]);
     }
 }
