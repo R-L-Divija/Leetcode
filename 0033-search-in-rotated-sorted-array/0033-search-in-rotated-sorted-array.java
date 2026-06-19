26907a794679943dc95a6a902a6ee33d34091595
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-19 19:56:22 +0530

    Time: 0 ms (100%), Space: 43.7 MB (64.51%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] arr, int t) {
+        int l=0;
+        int r=arr.length-1;
+
+        while(l<=r){
+            int m=l+(r-l)/2;
+
+            if(arr[m]==t)return m;
+
+            if(arr[l]<=arr[m]){
+            if(arr[l]<=t&&t<arr[m]){
+                r=m-1;
+            }else l=m+1;
+            }
+            else{
+                if(arr[m]<t && t<=arr[r]){
+                    l=m+1;
+                }else r=m-1;
+            }
+        }
+    return -1;
+    }
+}
