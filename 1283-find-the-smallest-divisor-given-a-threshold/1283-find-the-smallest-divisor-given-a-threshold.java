cb9bcee37c988f92ed59947ef03642588d6476e1
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-22 11:07:10 +0530

    Time: 6 ms (99.03%), Space: 51.3 MB (15.71%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,29 +1,25 @@
 class Solution {
-
-public boolean func(int a[],int tar,int m){
-    int s=0;
-    int t=0;
-    for(int x:a){
-   t=(x+m-1)/m;
-   s+=t;
-    }
-    return s<=tar;
-}
-    public int smallestDivisor(int[] a, int t) {
-        int l=1;
-        int h=0;
+    public boolean func(int a[],int t,int m){
+        int s=0;
+      
         for(int x:a){
-            h=Math.max(h,x);
+            s+=(x+m-1)/m;
+          
         }
-        while(l<h){
-            int mid=l+(h-l)/2;
-            if(func(a,t,mid)){
-                h=mid;
-            }else{
-                l=mid+1;
-            }
-
+        return s<=t;
+    }
+    public int smallestDivisor(int[] nums, int t) {
+        int l=1;
+        int r=0;
+        int a=0;
+        for(int x:nums)r=Math.max(r,x);
+        while(l<=r){
+           int m=l+(r-l)/2;
+           if(func(nums,t,m)){
+            a=m;
+            r=m-1;
+           }else l=m+1;
         }
-        return l;
+        return a;
     }
 }
