ba7266d429c289dc1fd6c5126a5ae9204502004f
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-06-23 14:19:50 +0530

    Time: 0 ms (100%), Space: 43.8 MB (61.03%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean searchMatrix(int[][] m, int t) {
+        int l=0;
+        int r=m[0].length-1;
+        while(l<m.length && r>=0){
+            if(t==m[l][r])return true;
+            else if(t<m[l][r])r--;
+            else l++;
+        }
+        return false;
+    }
+}
