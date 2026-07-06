79166739775c83ee742081ff538fd9cc237be58b
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-07-06 20:31:05 +0530

    Time: 41 ms (7.04%), Space: 47.4 MB (11.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,32 +1,34 @@
 class Solution {
-    public int coinChange(int[] a, int t) {
-      int n=a.length;
-      if(t==0)return 0;
-      int dp[][]=new int[a.length][t+1];
-      for(int i=0;i<a.length;i++){
-       Arrays.fill(dp[i],Integer.MAX_VALUE);
-      }
-      for(int r=0;r<n;r++){
-        dp[r][0]=0;
-      }
-    for(int j=0;j<=t;j++){
-        if(j % a[n-1] == 0){
-                dp[n-1][j] = j / a[n-1];
-            }
+    public int solve(int i, int s, int a[], int tar, int dp[][]) {
+        if (s == tar) return 0;
+        if (i == a.length) return Integer.MAX_VALUE;
+
+        if (dp[i][s] != -1) return dp[i][s];
+
+        int t = Integer.MAX_VALUE;
+
+        if (s  <= tar- a[i]) {
+            int res = solve(i, s + a[i], a, tar, dp);
+
+            if (res != Integer.MAX_VALUE)
+                t = 1 + res;
         }
-      for(int i=n-2;i>=0;i--){
-        for(int j=0;j<=t;j++){
-            //not take
-            int nt=dp[i+1][j];
-            //take;
-            int ta=Integer.MAX_VALUE;
-            if(j-a[i]>=0 &&dp[i][j-a[i]]!=Integer.MAX_VALUE){
-            ta=1+dp[i][j-a[i]];
-            }
-            dp[i][j]=Math.min(ta,nt);
-        }
-      }
-      if(dp[0][t]>=Integer.MAX_VALUE)return -1;
-      return dp[0][t];
+
+        int nt = solve(i + 1, s, a, tar, dp);
+
+        return dp[i][s] = Math.min(t, nt);
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+
+        int dp[][] = new int[coins.length][amount + 1];
+
+        for (int i = 0; i < coins.length; i++)
+            Arrays.fill(dp[i], -1);
+
+        int ans = solve(0, 0, coins, amount, dp);
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
