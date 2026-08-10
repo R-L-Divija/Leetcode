8ab2eec62017f51307af16a7481328beb977037f
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-10 10:03:31 +0530

    Time: 2 ms (99.75%), Space: 43.5 MB (70.53%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -3,8 +3,8 @@ class Solution {
         StringBuilder s1=new StringBuilder();
         int op=0;
         for(char c:s.toCharArray()){
-            if(c=='('&&op++>0)s1.append('(');
-              if(c==')'&&op-->1)s1.append(')');
+            if(c=='('&& op++>0)s1.append('(');
+            if(c==')'&&op-->1)s1.append(')');
         }
         return s1.toString();
     }
