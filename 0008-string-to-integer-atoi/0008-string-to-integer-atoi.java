36529ece651815cd9d2062c616d8aedd84712a24
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-11 14:20:02 +0530

    Time: 1 ms (100%), Space: 43.7 MB (75.86%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,22 +1,25 @@
 class Solution {
+    public int func(String s,int i,long num,int f){
+        if(i==s.length() || !Character.isDigit(s.charAt(i))){
+            return (int)num*f;
+        }
+        int d=s.charAt(i)-'0';
+          num=num*10+d;
+        if(num*f >Integer.MAX_VALUE)return Integer.MAX_VALUE;
+        if(num*f<Integer.MIN_VALUE)return Integer.MIN_VALUE;
+      
+        return func(s,i+1,num,f);
+    }
     public int myAtoi(String s) {
-        int i=0;
-        
-        int n=s.length();
-        while(i<n && s.charAt(i)==' ')i++;
-        int f=1;
-        if(i<n &&(s.charAt(i)=='+' ||s.charAt(i)=='-')){
-            if(s.charAt(i)=='-')f=-1;
-                i++;
-            
-        }
-        long s1=0;
-        while(i<n && Character.isDigit(s.charAt(i))){
-           s1=s1*10+(s.charAt(i)-'0');
-           if(s1*f<Integer.MIN_VALUE)return Integer.MIN_VALUE;
-           if(s1*f>Integer.MAX_VALUE)return Integer.MAX_VALUE;
-           i++;
-        }
-        return (int)s1*f;
+       int i=0;
+       int n=s.length();
+       while(i<n && s.charAt(i)==' ')i++;
+int f=1;
+       if(i<n &&( s.charAt(i)=='+'||s.charAt(i)=='-')){
+        if(s.charAt(i)=='-'){
+f=-1;
+        }i++;
+       }
+       return func(s,i,0,f);
     }
 }
