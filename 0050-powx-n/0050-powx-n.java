0cf1b34f95fead961f8bb168462af79e0b922135
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-11 14:50:10 +0530

    Time: 0 ms (100%), Space: 48.4 MB (11.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,17 @@
 class Solution {
+    public double func(double x,long n){
+        if(n==0)return 1.0;
+        double half=func(x,n/2);
+    
+       if(n%2==0)return half*half;
+     
+        return half*half*x;
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N=n;
+        if(n<0){
+             return 1.0/func(x,-N);
+        }
+        return func(x,N);
     }
 }
