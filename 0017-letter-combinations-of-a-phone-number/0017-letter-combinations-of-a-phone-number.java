ad718646fa0520f98f514d167c997230344351f9
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-15 14:46:09 +0530

    Time: 0 ms (100%), Space: 43.1 MB (95.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,25 +1,23 @@
 class Solution {
-    List<String>ans;
-    StringBuilder temp;
-    String arr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public void func(int index,String digits){
-if(index==digits.length()){
-    ans.add(temp.toString());
-    return ;
-}
-   String pc = arr[digits.charAt(index) - '0'];
-    for(int i=0;i<pc.length();i++){
-        temp.append(pc.charAt(i));
-        func(index+1,digits);
-        temp.deleteCharAt(temp.length()-1);
+    public List<String>p;
+    public String arr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public void func(int index,String digits,StringBuilder temp){
+       if(index==digits.length()){
+        p.add(temp.toString());
+        return ;
+       }
+        String pc=arr[digits.charAt(index)-'0'];
+        for(int i=0;i<pc.length();i++){
+            temp.append(pc.charAt(i));
+            func(index+1,digits,temp);
+            temp.deleteCharAt(temp.length()-1);
+        }
+        if(pc.length()==0)func(index+1,digits,temp);
+        
     }
-    if(pc.length()==0)func(index+1,digits);
-}
-    
     public List<String> letterCombinations(String digits) {
-        ans=new ArrayList<>();
-        temp=new StringBuilder();
-        func(0,digits);
-        return ans;
+        p=new ArrayList<>();
+        func(0,digits,new StringBuilder());
+        return p;
     }
 }
