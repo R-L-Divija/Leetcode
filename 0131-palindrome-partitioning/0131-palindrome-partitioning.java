de16bd26c4bbb48361fcc83a5c9c7413b0c46d72
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-17 16:04:54 +0530

    Time: 14 ms (6.82%), Space: 66 MB (5.96%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,31 +1,28 @@
 class Solution {
-    List<List<String>>ans;
-    List<String>temp;
-    
-     public boolean isP(String s){
-        String rev=new StringBuilder(s).reverse().toString();
-        if(s.equals(rev))return true;
-        return false;
-     }
-    public void func(int index,String s){
+    List<List<String>>p;
+    public boolean isP(String s){
+        StringBuilder s1=new StringBuilder(s).reverse();
+       
+        return s1.toString().equals(s);
+    }
+    public void func(int index,String s,List<String>temp){
+
         if(index==s.length()){
-            ans.add(new ArrayList<>(temp));
-            return ;
+            p.add(new ArrayList<>(temp));
+            return;
         }
         for(int i=index;i<s.length();i++){
             String subs=s.substring(index,i+1);
             if(isP(subs)){
-            temp.add(subs);
-            func(i+1,s);
-            temp.remove(temp.size()-1);
-        }
+                temp.add(subs);
+                func(i+1,s,temp);
+                temp.remove(temp.size()-1);
+            }
         }
     }
-
     public List<List<String>> partition(String s) {
-       ans=new ArrayList<>();
-       temp=new ArrayList<>();
-       func(0,s);
-       return ans; 
+       p=new ArrayList<>();
+       func(0,s,new ArrayList<>());
+       return p; 
     }
 }
