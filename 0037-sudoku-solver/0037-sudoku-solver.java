9c5361807850c9658298a67f0dd30726131a3786
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-19 11:45:40 +0530

    Time: 221 ms (66.04%), Space: 43.2 MB (9.77%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,38 +1,38 @@
 class Solution {
-    public boolean check(int r, int c, char[][]board,char ch){
-        //row check
+    public boolean check(int r,int c,char c1,char[][]b){
         for(int i=0;i<9;i++){
-            if(board[r][i]==ch)return false;
+            if(b[i][c]==c1)
+            return false;
         }
-        //col check
-          for(int i=0;i<9;i++){
-            if(board[i][c]==ch)return false;
+        for(int i=0;i<9;i++){
+            if(b[r][i]==c1)
+            return false;
         }
-        //box check
         int fr=(r/3)*3;
         int fc=(c/3)*3;
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-               int nr=fr+i,nc=fc+j;
-               if(board[nr][nc]==ch)return false;
+               int nr=fr+i;
+               int nc=fc+j;
+               if(b[nr][nc]==c1)
+               return false;
             }
         }
-        return true;
+           return true;
     }
-public boolean func(int r,int c,char[][] board){
-    if(r==9)return true;
-    if(c==9) return func(r+1,0,board);
-    if(board[r][c]!='.')return func(r,c+1,board);
-    for(char ch='1';ch<='9';ch++){
-            if(check(r,c,board,ch)){
-            board[r][c]=ch;
-            if(func(r,c+1,board))return true;
-            board[r][c]='.';
-            } 
-        }
-        return false;
+    public boolean func(int r,int c,char[][] board){
+            if(r==9) return true;
+            if(c==9) return func(r+1,0,board);
+            if(board[r][c]!='.')return func(r,c+1,board);
+             for(char i='1';i<='9';i++){
+                if(check(r,c,i,board)){
+                    board[r][c]=i;
+                    if(func(r,c+1,board))return true;
+                    board[r][c]='.';
+                }
+             }
+             return false;
     }
-
     public void solveSudoku(char[][] board) {
         func(0,0,board);
     }
