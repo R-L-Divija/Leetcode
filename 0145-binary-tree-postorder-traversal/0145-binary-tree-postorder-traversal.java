9face590caee5f35291da892c658204a3ad88e9a
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2026-08-24 12:38:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (15.35%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,16 +14,27 @@
  * }
  */
 class Solution {
-    List<Integer>res;
-    public void func(TreeNode root){
-        if(root==null)return;
-        func(root.left);
-        func(root.right);
-        res.add(root.val);
-    }
     public List<Integer> postorderTraversal(TreeNode root) {
-        res=new ArrayList<>();
-        func(root);
-        return res;
+        List<Integer>ans=new ArrayList<>();
+       if(root==null)return ans;
+        Stack<TreeNode>st1=new Stack<>();
+        Stack<TreeNode>st2=new Stack<>();
+
+        st1.push(root);
+        while(!st1.isEmpty()){
+            TreeNode temp=st1.pop();
+            st2.push(temp);
+            if(temp.left!=null){
+                st1.push(temp.left);
+            }
+            if(temp.right!=null){
+                st1.push(temp.right);
+           
+            }
+        }
+        while(!st2.isEmpty()){
+            ans.add(st2.pop().val);
+        }
+ return ans;
     }
 }
